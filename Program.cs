1ee6ef419c2fac621967c13929a49bef76e62fae
bartashchuk <bartashchuk.gh@gmail.com>
2024-11-30 15:09:33 +0200

    Added + on line 9

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace TestGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Teacher!"); // ---
+            Console.WriteLine("Hello Teacher!"); // +++
         }
     }
 }
